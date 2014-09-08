6403d1372d02816a5104d77bcda1d65e6fa0e3de
Nuatu Tseggai & Sterling Houghton <student@epicodus.com>
2014-09-08 13:58:24 -0700

    Add post model and spec.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908191627) do
+ActiveRecord::Schema.define(version: 20140908205005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string  "title"
+    t.string  "content"
+    t.integer "qty_likes"
+  end
 
   create_table "users", force: true do |t|
     t.string "name"
